e767109422fbecce11bed237abf02e5c8e2383d9
kimsubin <chocobingsu81@naver.com>
2022-05-02 10:36:49 +0900

    2번째

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
-	cout << "HelloWorld" << endl;
+	int MyMoney = 100;
+	int Operand = 10;
+
+	cout << MyMoney + Operand << endl;
+	cout << MyMoney * Operand << endl;
+	cout << MyMoney - Operand << endl;
+	cout << MyMoney / Operand << endl;
+	cout << MyMoney % Operand << endl;
+	cout << 10 + (2 * 20) << endl;
+
+
+	float MyR = 0.1f;
+	float MyG = 0.3f;
+	float MyB = 0.2f;
+
+	float Graycolor = 0.299f * MyR + 0.537f * MyG + 0.114f * MyB;
+
+	cout << Graycolor << endl;
+
+
 	return 0;
 }
